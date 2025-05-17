0231ceace552e12e99652c667762fde3934cda1f
volkan.tutunculer <volkan.tutunculer@gmail.com>
2025-05-17 15:50:51 -0400

    try1

--- cypress/e2e/integration/login-tests.cy.js
+++ cypress/e2e/integration/login-tests.cy.js
@@ -1,25 +1,21 @@
-/// <reference types ="cypress"/>
-
+/// <reference types="cypress"/>
 import BasePage from "../../pages/BasePage";
-import LoginPage from "../../pages/loginPage";
+import LoginPage from "../../pages/LoginPage";
 
-describe("Login Verification", () => {
+describe('Login Verification', () => {
+
   const loginPage = new LoginPage();
   const basePage = new BasePage();
 
-it("Validate Login with valid credentials", () => {
+  it('Validate login with valid credentials', () => {
     cy.visit(Cypress.env('APP_BASE_URL'));
 
-    loginPage.login(Cypress.env("USERNAME"), Cypress.env("PASSWORD"));
+    loginPage.login(Cypress.env('USERNAME'), Cypress.env('PASSWORD'));
 
-    cy.url().should("include", "Web Orders");
+    cy.url().should('include', 'weborders');
 
-    basePage.getWebOrdersHeading().should("have.text", "Web Orders");
-
-    basePage.getLogoutButton().should("have.text", "Logout");
-
-    basePage
-      .getWelcomeUserInfo()
-      .should("include.test", Cypress.env("USERNAME"));
-  });
+    basePage.getWebOrdersHeading().should('have.text', 'Web Orders');
+    basePage.getLogoutButton().should('have.text', 'Logout');
+    basePage.getWelcomeUserInfo().should('include.text', Cypress.env('USERNAME'));
+  })
 });
