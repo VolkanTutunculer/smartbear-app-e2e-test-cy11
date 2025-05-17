4a8e868a32ce5ee761f36f4d9eab11afd6b95c5d
volkan.tutunculer <volkan.tutunculer@gmail.com>
2025-05-17 15:41:51 -0400

    Initial commit

---
+++ cypress/e2e/integration/login-test.cy.js
@@ -0,0 +1,27 @@
+/// <reference types ="cypress"/>
+
+import BasePage from "../../pages/BasePage";
+import LoginPage from "../../pages/loginPage";
+
+describe("Login Verification", () => {
+  const loginPage = new LoginPage();
+  const basePage = new BasePage();
+
+  it("Validate Login with valid credentials", () => {
+    cy.visit(Cypress.env('APP_BASE_URL'));
+
+    loginPage.login(Cypress.env("USERNAME"), Cypress.env("PASSWORD"));
+
+    cy.url().should("include", "weborders");
+
+    basePage.getWebOrdersHeading().should("have.text", "Web Orders");
+
+    basePage.getLogoutButton().should("have.text", "Logout");
+
+    basePage
+      .getWelcomeUserInfo()
+      .should("include.test", Cypress.env("USERNAME"));
+  });
+});
+
+
