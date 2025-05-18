14a15ca389ab5f7a3b15069779f742e5ea4e9744
volkan.tutunculer <volkan.tutunculer@gmail.com>
2025-05-18 11:40:53 -0400

    beforeEach added

--- cypress/e2e/integration/login-tests.cy.js
+++ cypress/e2e/integration/login-tests.cy.js
@@ -7,8 +7,12 @@ describe('Login Verification', () => {
   const loginPage = new LoginPage();
   const basePage = new BasePage();
 
+  beforeEach(() =>{
+    cy.visit(Cypress.env("APP_BASE_URL"));
+  })
+
   it('Validate login with valid credentials', () => {
-    cy.visit(Cypress.env('APP_BASE_URL'));
+ 
 
     loginPage.login(Cypress.env('APP_USERNAME'), Cypress.env('PASSWORD'));
 
@@ -20,7 +24,7 @@ describe('Login Verification', () => {
   })
 
     it("TG11S-T142 - Validate logout", () => {
-    cy.visit(Cypress.env("APP_BASE_URL"));
+  
     loginPage.login(Cypress.env("APP_USERNAME"), Cypress.env("PASSWORD"));
     basePage.getLogoutButton().click();
     cy.url().should("include", "Login");
@@ -29,24 +33,27 @@ describe('Login Verification', () => {
 
   [
     {
+      title:'empty creds',
       username: "",
       password: "",
     },
     {
+      title:'invalid Username Valid Password',
       username: "InvalidUsername",
       password: Cypress.env("PASSWORD"),
     },
-    {
+    {title:'valid Username Invalid Password',
       username: Cypress.env("APP_USERNAME"),
       password: "InvalidPassword",
     },
     {
+      title:'invalid Username Invalid Password',
       username: "InvalidUsername",
       password: "InvalidPassword",
     },
   ].forEach((creds) => {
-    it(`TG11S-T164 - Validate login with invalid credentials ${creds.username} ${creds.password}`, () => {
-      cy.visit(Cypress.env("APP_BASE_URL"));
+    it(`TG11S-T164 - Validate login with invalid credentials ${creds.title}`, () => {
+      
 
       if(creds.username === '' && creds.password === '') loginPage.clickOnLoginButton();
       else loginPage.login(creds.username, creds.password);
